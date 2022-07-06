b161b4ce91bafb742b4936a9a82ade7872ba75eb
Denis Lazarenko <dentus25@gmail.com>
2022-07-06 21:22:05 +0300

    add_components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,57 +5,67 @@ import { nanoid } from 'nanoid';
 import Section from './Section';
 import ContactsForm from './ContactsForm';
 import ContactList from './ContactList';
+import Filter from './Filter';
 
-import styles from './app.module.css'
+import styles from './app.module.css';
 
 export function App() {
-  
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
   const firstRender = useRef(true);
 
   useEffect(() => {
-    const items = JSON.parse(localStorage.getItem("contacts"))
+    const items = JSON.parse(localStorage.getItem('contacts'));
     if (items?.length) {
       setContacts(items);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (!firstRender.current) {
-      localStorage.setItem("contacts", JSON.stringify(contacts));
-    }
-    else {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    } else {
       firstRender.current = false;
     }
-  }, [contacts])
+  }, [contacts]);
 
-  function addContacts ({ name, number }) {
-    const newContact = {
-      name,
-      number,
-      id: nanoid(),
-    };
-    setContacts(prevState => {
-      const findName = contacts.find(el => el.name === name);
-      if (!findName) {
-        return [...prevState, newContact]
-      } else {
-        alert(`${newContact.name} is already in contacts`)
-      }
-    });
-  };
+  const addContacts = useCallback(
+    ({ name, number }) => {
+      const newContact = {
+        name,
+        number,
+        id: nanoid(),
+      };
+      setContacts(prevState => {
+        const findName = contacts.find(el => el.name === name);
+        if (!findName) {
+          return [...prevState, newContact];
+        } else {
+          alert(`${newContact.name} is already in contacts`);
+        }
+      });
+    },
+    [contacts]
+  );
 
-  const removeContacts = useCallback((id) => {
-    setContacts( prevContacts => prevContacts.filter(contact => contact.id !== id));
-  }, [setContacts])
+  const removeContacts = useCallback(
+    id => {
+      setContacts(prevContacts =>
+        prevContacts.filter(contact => contact.id !== id)
+      );
+    },
+    [setContacts]
+  );
 
-  const handleFilter = useCallback(({ target }) => {
-   return setFilter(target.value);
-  }, [setFilter])
+  const handleFilter = useCallback(
+    ({ target }) => {
+      return setFilter(target.value);
+    },
+    [setFilter]
+  );
 
-  function getFiltredContacts() {
+  const getFiltredContacts = useCallback(() => {
     if (!filter) {
       return contacts;
     }
@@ -65,27 +75,21 @@ export function App() {
       return nameValue.includes(filterValue);
     });
     return filterContacts;
-  };
+  }, [contacts, filter]);
 
   const contact = getFiltredContacts();
 
   return (
-      <div className={styles.container}>
-        <Section title="Phonebook">
-          <ContactsForm onSubmit={addContacts} />
-        </Section>
-        <Section title="Contacts">
-          <div className={styles.wrap}>
-             <label className={styles.label}>
-            Find contacts by name
-            <input className={styles.input} onChange={handleFilter} type="text" name="filter" />
-          </label>
-         </div>
-          <ContactList contact={contact} removeContacts={removeContacts} />
-        </Section>
-      </div>
-    );
-
+    <div className={styles.container}>
+      <Section title="Phonebook">
+        <ContactsForm onSubmit={addContacts} />
+      </Section>
+      <Section title="Contacts">
+        <Filter onChange={handleFilter} />
+        <ContactList contact={contact} removeContacts={removeContacts} />
+      </Section>
+    </div>
+  );
 }
 
 // export class App extends Component {
